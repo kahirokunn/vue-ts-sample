9bd086e9bd6e186ffbb8805de3552c20f4601845
k-okina <okinakahiro@gmail.com>
2020-02-25 17:07:58 +0900

    step1 型エラー

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg="showAge()" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import Vue from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
+export default Vue.extend({
   name: "Home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      age: 123
+    };
+  },
+  computed: {
+    test() {
+      return this.age;
+    }
+  },
+  methods: {
+    showAge() {
+      return this.test;
+    }
   }
-};
+});
 </script>
